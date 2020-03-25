19064e01e47cdd06e29936c64ee396e3fcbc0c8b
Piotr Janiszewski <janiszewskipeter@gmail.com>
2020-03-25 12:09:46 +0100

    Fully working game!

--- battleship.py
+++ battleship.py
@@ -1,38 +1,101 @@
 
+import os
+
+# check input fun
+# round menu fun
+# sink mark fun
+# wait screen inbetween switch players
 def main():
-    board = []
-    init_board(board)
-    print_board(board)
+    board1 = init_board()
+    board2 = init_board()
+    shoot_board1 = init_board()
+    shoot_board2 = init_board()
     print_menu()
     is_running = True
     ship = 1
+    mark = ""
     previous_move = []
     current_player = 1
+    is_shooting = True
     while is_running:
+        mark = "X"
+        if current_player == 1:
+            print_board(board1)
+            board = board1
+        else:
+            print_board(board2)
+            board = board2
         coordinates = user_input()
-        if not user_input_check(coordinates) :
+        os.system('cls||clear')
+        if not user_input_check(coordinates):
             continue
         translated_cooridinates = translate_letters_to_numbers(coordinates)
         if not place_is_aviable(board, translated_cooridinates):
+            print("Place is already taken")
             continue
         if ship == 2:
             if place_is_aviable_next_part(board, previous_move, translated_cooridinates):
                 continue
         if ship == 3:
             if not place_is_aviable_next_part(board, previous_move, translated_cooridinates):
-                print("Dude only bertical or horizontal ships!")
+                print("Dude only vertical or horizontal ships!")
                 continue
-        mark_player_coordinates(board, translated_cooridinates)
+        mark_player_coordinates(board, translated_cooridinates, mark)
         previous_move = translated_cooridinates
         ship += 1
-        print_board(board)
-        if board.count('X') == 3:
-            switch_player(current_player)
+        
+        if sum(x.count('X') for x in board) == 3:
+            print_board(board)
+            if current_player == 2:
+                is_running = False
+            current_player = switch_player(current_player)
+            os.system("""bash -c 'read -s -n 1 -p "Press any key to continue..."'""")
+            os.system('cls||clear')
+            print("Current player for shooting round is player: ", current_player)
+    
+    while is_shooting:
+        if current_player == 1:
+            print_board(shoot_board1)
+            board = shoot_board1
+        else:
+            print_board(shoot_board2)
+            board = shoot_board2
+        coordinates = user_input()
+        if not user_input_check(coordinates):
+            continue
+        translated_cooridinates = translate_letters_to_numbers(coordinates)
+        if current_player == 1:
+            board = board2
+        else:
+            board = board1
+
+        if not place_is_aviable(board, translated_cooridinates):
+            mark = "H"
+            if current_player == 1:
+                board = shoot_board1
+            else:
+                board = shoot_board2
+            mark_player_coordinates(board, translated_cooridinates, mark)
+            win_check(current_player, board)
+            
+        else:
+            mark = "M"
+            if current_player == 1:
+                board = shoot_board1
+            else:
+                board = shoot_board2
+                print("current player", current_player)
+            mark_player_coordinates(board, translated_cooridinates, mark)
+            print("current player", current_player)
+            current_player = switch_player(current_player)
+            print("current player", current_player)
 
 
-def init_board(board):
+def init_board():
+    board = []
     for x in range(0, 5):
         board.append(["0"]*5)
+    return board
 
 
 def print_menu():
@@ -76,7 +139,6 @@ def place_is_aviable(board, coordinates):
     if board[coordinates[0]][coordinates[1]] == '0':
         return True
     else:
-        print("Place already taken")
         return False
 
 
@@ -129,12 +191,13 @@ def switch_player(current_player):
         current_player = 2
     else:
         current_player = 1
+    return current_player
 
 
-def mark_player_coordinates(board, coordinates):
-    board[coordinates[0]][coordinates[1]] = 'X'
+def mark_player_coordinates(board, coordinates, mark):
+    board[coordinates[0]][coordinates[1]] = mark
     return board
-
+    
 
 def check_ship_end():
     pass
@@ -153,10 +216,12 @@ def mark_shoot(coordinates, board):
     return board
 
 
-def win_check():
-    pass
-
+def win_check(current_player, board):
+    if sum(x.count('H') for x in board) == 3:
+        print("Congratulations player", current_player, "has won the game")
+        exit()
 
 if __name__ == '__main__':
     main()
+
 
